998912b7710b31130b72a2585b1f3ab377246935
Andrew Gibson <andrew@goofballLogic.com>
2019-07-31 21:39:48 +0100

    initial commit

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+}
